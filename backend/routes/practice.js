ecbbd89e28685bad1fe576a251991da96c9d8d68
multi-vit <103585624+multi-vit@users.noreply.github.com>
2022-10-05 18:05:34 +0100

    Add POST route

--- backend/routes/practice.js
+++ backend/routes/practice.js
@@ -9,8 +9,55 @@ router.get("/", async function (req, res, next) {
   res.json({ body: response });
 });
 
-router.post("/", function (req, res, next) {
-  res.json({ message: "Here's what we received", body: req.body });
+router.post("/", async function (req, res, next) {
+  if (!req.body.date || !req.body.length) {
+    res.json({
+      message: "Items missing, here's what we received",
+      body: req.body,
+    });
+  } else {
+    //Check if length is number
+    if (isNaN(req.body.length) || req.body.length < 1) {
+      res.json({
+        message: "Length invalid, here's what we received",
+        body: req.body,
+      });
+    } else {
+      //Build the document if piece is present
+      let document = { date: req.body.date, length: req.body.length };
+      if (req.body.practiced) {
+        document.practiced = req.body.practiced;
+      }
+      if (
+        (req.body.piece &&
+          req.body.piece.title &&
+          req.body.piece.title.length > 0) ||
+        (req.body.piece &&
+          req.body.piece.composer &&
+          req.body.piece.composer.length > 0)
+      ) {
+        //If either title or composer is present, add them or if both, add both
+        if (req.body.piece.title) {
+          document.piece = { title: req.body.piece.title };
+        }
+        if (req.body.piece.composer) {
+          document.piece = { composer: req.body.piece.composer };
+        }
+        if (req.body.piece.title && req.body.piece.composer) {
+          document.piece = {
+            title: req.body.piece.title,
+            composer: req.body.piece.composer,
+          };
+        }
+      }
+      //Insert the document to the DB
+      let response = await practices.insertOne(document);
+      console.log("Response received from DB");
+      console.log(response);
+      //Respond
+      res.json({ message: "Response received from DB", body: response });
+    }
+  }
 });
 
 export default router;
